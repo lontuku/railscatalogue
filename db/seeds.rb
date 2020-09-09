4611071883687c5044972d7d9f239460805cead3
Tomas K.T <tomas.konig.t@gmail.com>
2020-09-09 02:45:18 -0300

    Se carga mas seed mas imágenes para ser transferidas a cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+arr = ["palosanto_", "ciervadesign", "_chomba", "loretocorreadiseno", "108label", "bethchile", "camisetaskuku", "_vittorio_rosso", "indomitagrafica", "veta.serigrafia", "lo.lisaolave", "la.pava", "dilema.cl", "paulamar.cl", "trinidadestudio", "jovivestuario", "anaranja.prendas", "andreadelahorrasastre", "siempre.oveja", "relmu_tejidos", "nal.knits", "lanitas_coloradas", "tune.yarns", "eter.clothing", "artificiozerodesecho", "nd_denim", "freckle_and_fox", "deshojandohilo"]
+
+arr.each do |name|
+    s = Store.create!(
+        name: "#{name}",
+        instagram_link: "https://www.instagram.com/#{name}",
+        user_id: 2,
+        category_id: 2)
+    s.images.attach(
+        io: File.open("db/pics/#{name}0.jpg"),
+        filename: "#{name}0")
+    s.images.attach(
+        io: File.open("db/pics/#{name}1.jpg"),
+        filename: "#{name}1")    
+end
