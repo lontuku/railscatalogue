0321424f49f25e333941e93c712728813008b68d
Tomas K.T <tomas.konig.t@gmail.com>
2020-09-10 13:38:44 -0300

    Se agregan tiendas de alta costura

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 # arr = ["palosanto_", "ciervadesign", "_chomba", "loretocorreadiseno", "108label", "bethchile", "camisetaskuku", "_vittorio_rosso", "indomitagrafica", "veta.serigrafia", "lo.lisaolave", "la.pava", "dilema.cl", "paulamar.cl", "trinidadestudio", "jovivestuario", "anaranja.prendas", "andreadelahorrasastre", "siempre.oveja", "relmu_tejidos", "nal.knits", "lanitas_coloradas", "tune.yarns", "eter.clothing", "artificiozerodesecho", "nd_denim", "freckle_and_fox", "deshojandohilo"]
 
-arr = [{:name=>"agrupa.cl", :total_post=>559, :total_follower=>559, :total_following=>385, :avg=>57}, {:name=>"decatalogo", :total_post=>2209, :total_follower=>2209, :total_following=>3117, :avg=>382}, {:name=>"ec_nalca", :total_post=>351, :total_follower=>351, :total_following=>122, :avg=>75}, {:name=>"feria30kilates", :total_post=>203, :total_follower=>203, :total_following=>316, :avg=>132}, {:name=>"local9f", :total_post=>2229, :total_follower=>2229, :total_following=>2460, :avg=>21}, {:name=>"tiendamakers", :total_post=>723, :total_follower=>723, :total_following=>1487, :avg=>48}]
+arr = [{:name=>"danielabozza", :total_post=>1011, :total_follower=>1011, :total_following=>1233, :avg=>504}, {:name=>"franciscatuca", :total_post=>314, :total_follower=>314, :total_following=>1462, :avg=>74}, {:name=>"galgastudio", :total_post=>400, :total_follower=>400, :total_following=>4423, :avg=>197}, {:name=>"maritaproustbecker", :total_post=>166, :total_follower=>166, :total_following=>2854, :avg=>218}, {:name=>"munaysisters", :total_post=>377, :total_follower=>377, :total_following=>649, :avg=>154}, {:name=>"saviatextil", :total_post=>215, :total_follower=>215, :total_following=>2247, :avg=>115}, {:name=>"shopsisa", :total_post=>1217, :total_follower=>1217, :total_following=>1010, :avg=>200}, {:name=>"studio_zenda", :total_post=>190, :total_follower=>190, :total_following=>211, :avg=>63}, {:name=>"zurita_designstudio", :total_post=>76, :total_follower=>76, :total_following=>977, :avg=>104}]
 
 arr.each do |h|
     nombre = h[:name]
@@ -16,7 +16,7 @@ arr.each do |h|
         name: "#{nombre}",
         instagram_link: "https://www.instagram.com/#{nombre}",
         user_id: 2,
-        category_id: 8,
+        category_id: 9,
         instagram_like_avg: h[:avg],
         instagram_follower_total: h[:total_follower],
         instagram_post_total: h[:total_post],
