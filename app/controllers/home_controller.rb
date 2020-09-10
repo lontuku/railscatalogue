76ec976b2e85cf41cc11356be2eb66d6e0e13064
Tomas K.T <tomas.konig.t@gmail.com>
2020-09-10 18:42:07 -0300

    Se hacen cambios en el diseño de las tarjetas para mostrar mas informacion

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,17 +2,26 @@ class HomeController < ApplicationController
   def index
     @user = current_user
     @stores = Store.all
+
+
     @categories = Category.all
     #@likes = Like.all
     cat = params[:categ]
     #flash.alert = "User not found."
     if !cat.nil?
       @stores = Store.where(:category_id => cat)
-    else
-      @stores = Store.by_id
+      
+    elsif params[:content].present?
+      item = params[:content]
+
+      @stores = Store.where('name LIKE ?', "%#{item}%" )
+    else 
+     @stores = Store.by_id
     end
 
     def stats
     end 
+
+    
   end
 end
